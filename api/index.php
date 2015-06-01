23d3f3e5bfd18bba6d0979399657bc5ca6141941
Manuel Torres Gil <mtorres@ual.es>
2015-06-01 14:37:49 +0200

    index.php

--- api/index.php
+++ api/index.php
@@ -8,6 +8,8 @@ $app->get('/api', 'home');
 $app->get('/api/amenity/{amenity}', 'findAmenities'); // curl -i -X GET http://localhost/MongoDBBlog/api/api/id/50ab0f8bbcf1bfe2536dc3f8
 $app->get('/api/bar/{bar}', 'findBar'); // curl -i -X GET http://localhost/MongoDBBlog/api/api/tag/trade
 $app->get('/api/amenity/{amenity}/lat/{lat}/lon/{lon}', 'findAmenities100MetersNear'); // curl -i -X POST -d '{"body":"Lore ipsum", "permalink": "TqoHkbHyUgLyCKWgPLqm", "author": "machine", "title": "Lore ipsum", "tags": ["Lore", "ipsum"], "comments":[{"body": "Lore ipsum", "email": "john@doe.com", "author": "John Doe"}]}' http://localhost/MongoDBBlog/api/api
+$app->get('/api/llamadaXQ', 'llamadaXQ'); // 
+$app->get('/api/dameREST', 'dameREST'); // 
 $app->notFound('notFound');
 
 // Handlers
@@ -24,6 +26,7 @@ function home() {
 	echo '<tr><td>GET</td><td>/api/amenity/{amenity}</td><td>Devuelve los nodos del tipo de <i>amenity</i> proporcionada</td><td>curl -i -X GET http://localhost/OSM_REST/api/api/amenity/bar</td></tr>';
 	echo '<tr><td>GET</td><td>/api/bar/{bar}</td><td>Devuelve el nodo correspondiente al bar proporcionado</td><td>curl -i -X GET http://localhost/OSM_REST/api/api/bar/Cuore</td></tr>';
 	echo '<tr><td>GET</td><td>/api/amenity/{amenity}/lat/{lat}/lon/{lon}</td><td>Devuelve los nodos del tipo de <i>amenity</i> proporcionado que están a menos de 100 metros de la latitud y longitud proporcionadas</td><td>curl -i -X GET http://localhost/OSM_REST/api/api/amenity/bar/lat/36.8388993/lon/-2.464748</td></tr>';
+	echo '<tr><td>GET</td><td>/api/llamadaXQ</td><td>Hace una llamada a una consulta XQuery</td><td>curl -i -X GET http://localhost/OSM_REST/api/api/llamadaXQ</td></tr>';
 	echo '</table>';
 
 	echo '<a href = "../index.php">Volver a index</a>';
@@ -64,23 +67,39 @@ print XMLToJSON::Parse($fullurl);
 }
 
 //Updates values from its key
-function addComment($id) {
+function llamadaXQ() {
+	$fullurl = 'http://localhost:8080/exist/rest/db/apps/osm/saludar.xq';
 
-	// Access to the global var $app
-	global $app;
+	$ch = curl_init();
 
-	// Get the id of the post and convert it to an ObjectId 
-	// Queries using _id need it converted to a MongoId object
-	$objectId = new MongoId($id);
+// Establecer URL y otras opciones apropiadas
+curl_setopt($ch, CURLOPT_URL, $fullurl);
+curl_setopt($ch, CURLOPT_HEADER, 0);
 
-	// Connect to the database 
-	include_once("../connection.php");
+// Capturar la URL y pasarla al navegador
+curl_exec($ch);
 
-	// Obtain the data of the request
-	$requestData = json_decode($app->request->getRawBody());
+// Cerrar el recurso cURL y liberar recursos del sistema
+curl_close($ch);
+}
 
-	// Update the post adding the new comment passed on the request
-	$collection->update(array('_id' => $objectId), array('$push' => array('comments' => $requestData)));
+//Adds a new post
+function dameREST() {
+
+
+$fullurl = 'http://localhost:8080/exist/rest/db/osm?_query=restame:dameREST()';
+
+	$ch = curl_init();
+
+// Establecer URL y otras opciones apropiadas
+curl_setopt($ch, CURLOPT_URL, $fullurl);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+
+// Capturar la URL y pasarla al navegador
+curl_exec($ch);
+
+// Cerrar el recurso cURL y liberar recursos del sistema
+curl_close($ch);
 }
 
 function notFound() {
