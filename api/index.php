6cb1705c00be9ed473326e319fcc0d4af364e91b
Manuel Torres Gil <mtorres@ual.es>
2015-06-01 12:17:18 +0200

    Corregido error bd incorrecta

--- api/index.php
+++ api/index.php
@@ -47,7 +47,7 @@ function findBar ($bar) {
 
 include ('../XMLToJSON.php');
 
-$fullurl = 'http://localhost:8080/exist/rest/db/test?_query=%3CBars%3E%20{%20for%20$node%20in%20collection(%22/db/osm%22)/osm/node%20where%20$node/tag[@k=%22amenity%22]%20and%20$node/tag[@v=%22bar%22]%20and%20$node/tag[@k=%22name%22]%20and%20$node/tag[@v=%22' . $bar . '%22]%20return%20%3Cbar%3E%20{$node[@lat]}%20%3C/bar%3E%20}%20%3C/Bars%3E';
+$fullurl = 'http://localhost:8080/exist/rest/db/osm?_query=%3CBars%3E%20{%20for%20$node%20in%20collection(%22/db/osm%22)/osm/node%20where%20$node/tag[@k=%22amenity%22]%20and%20$node/tag[@v=%22bar%22]%20and%20$node/tag[@k=%22name%22]%20and%20$node/tag[@v=%22' . $bar . '%22]%20return%20%3Cbar%3E%20{$node[@lat]}%20%3C/bar%3E%20}%20%3C/Bars%3E';
 
 print XMLToJSON::Parse($fullurl);
 
@@ -58,7 +58,7 @@ function findAmenities100MetersNear($amenity, $lat, $lon) {
 
 include ('../XMLToJSON.php');
 
-$fullurl = 'http://localhost:8080/exist/rest/db/test?_query=%3CBars%3E%20{%20for%20$node%20in%20collection(%22/db/osm%22)/osm/node%20where%20$node/tag/@k=%22amenity%22%20and%20$node/tag/@v=%22' . $amenity . '%22%20and%20$node/@lat%20%3E%20' . $lat . '%20-%200.00000898%20*%20100%20and%20$node/@lat%20%3C%20' . $lat . '%20-%20-0.00000898%20*%20100%20and%20$node/@lon%20%3E%20' . $lon . '%20-%200.00000898%20*%20100%20and%20$node/@lon%20%3C%20' . $lon . '%20-%20-0.00000898%20*%20100%20return%20%3Cbar%3E%20{$node}%20%3C/bar%3E%20}%20%3C/Bars%3E';
+$fullurl = 'http://localhost:8080/exist/rest/db/osm?_query=%3CBars%3E%20{%20for%20$node%20in%20collection(%22/db/osm%22)/osm/node%20where%20$node/tag/@k=%22amenity%22%20and%20$node/tag/@v=%22' . $amenity . '%22%20and%20$node/@lat%20%3E%20' . $lat . '%20-%200.00000898%20*%20100%20and%20$node/@lat%20%3C%20' . $lat . '%20-%20-0.00000898%20*%20100%20and%20$node/@lon%20%3E%20' . $lon . '%20-%200.00000898%20*%20100%20and%20$node/@lon%20%3C%20' . $lon . '%20-%20-0.00000898%20*%20100%20return%20%3Cbar%3E%20{$node}%20%3C/bar%3E%20}%20%3C/Bars%3E';
 
 print XMLToJSON::Parse($fullurl);
 }
