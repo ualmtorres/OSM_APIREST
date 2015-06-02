44cbb32c7e20f65d9f589170e43f0e48d42cd228
Manuel Torres Gil <mtorres@ual.es>
2015-06-02 22:45:45 +0200

    Refactorizado

--- buscar.php
+++ buscar.php
@@ -1,53 +1,6 @@
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
-
-    <title>Narrow Jumbotron Template for Bootstrap</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="jumbotron-narrow.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-
-    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true"></script>
-    <script>
-function initialize(lat, lon) {
-  var myLatlng = new google.maps.LatLng(lat, lon);
-  var mapOptions = {
-    zoom: 4,
-    center: myLatlng
-  }
-  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-  var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      title: 'Hello World!'
-  });
-}
-
-/*google.maps.event.addDomListener(window, 'load', initialize);*/
-
-    </script>
-  </head>
-
-  <body>
+<?php
+  include('header.php');
+?>
 
     <div class="container">
       <div class="header clearfix">
@@ -107,13 +60,9 @@ function initialize(lat, lon) {
         </div>
       </div>
 
-      <footer class="footer">
-        <p>&copy; Espeshal Company 2015</p>
-      </footer>
-
-    </div> <!-- /container -->
-  </body>
-</html>
+<?php
+  include("tail.php");
+?>
  
 
 
---
+++ tail.php
@@ -0,0 +1,7 @@
+      <footer class="footer">
+        <p>&copy; Espeshal Company 2015</p>
+      </footer>
+
+    </div> <!-- /container -->
+  </body>
+</html>
